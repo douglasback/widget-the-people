c29928248009e6a2cb3f9377a9a95eedf0b289c6
Douglas Back <douglasback@mac.com>
2013-02-27 08:11:20 -0500

    JS cleanup

--- public/js/search.js
+++ public/js/search.js
@@ -1,45 +1,53 @@
-(function(){
-    var petitions,
-        petitionTitles;
-    
-    var getPetitionId = function(){
-        $('#form-search').on("submit", function(e){
-            e.preventDefault();
-            var needle = $('#title-search').val(),
-                iframe,
-                host;
-            var result = _.find(petitions, function(p){
-                return p.title === needle;
-            });
-            console.log("petition id === " + result.id);
-            //build iframe code
-            
-            iframe = '<iframe src="//'+ document.location.host + '/widget/' + result.id +'" style="width: 100%; height: 265px; border: 0;" scrolling="no"></iframe>';
-            $('#generated-widget').val(iframe);
-            // test preview
-            $('#preview-inner').html(iframe);
-        });
-    };
-    var loadPetitions = function(){
-        $('#spinner').fadeIn();
-             
-        $.getJSON('https://petitions.whitehouse.gov/api/v1/petitions.jsonp?&key=' + window.wtp.APIKEY + '&limit=1000&callback=?', function(data){
-            petitions = _.map(data.results, function(p){
-                var pet = {};
-                pet.title = p.title;
-                pet.id = p.id;
-                return pet;
-            });
-            petitionTitles = _.pluck(petitions, "title");
-            $('#title-search').typeahead({
-                source: petitionTitles,
-                minLength: 2
+(function($){
+  var petitions, // this will be an array containing the title and ID
+      petitionTitles, // used for the typeahead/autocomplete input
+      ENDPOINT = 'https://petitions.whitehouse.gov/api/v1/petitions.jsonp';
+  
+  var loadPetitions = function(){
+    $('#spinner').fadeIn();
 
-            });
-            $('#spinner').fadeOut();
-            
-        });
-    }
-    loadPetitions();
-    getPetitionId();
-}());
+    $.getJSON(ENDPOINT + '?&key=' + window.wtp.APIKEY + '&limit=1000&callback=?', function(data){
+      petitions = _.map(data.results, function(p){
+        var pet = {};
+        pet.title = p.title;
+        pet.id = p.id;
+        return pet;
+      });
+
+      //Attach petitionTitles to autocomplete widget
+      petitionTitles = _.pluck(petitions, "title");
+      $('#title-search').typeahead({
+        source: petitionTitles,
+        minLength: 2
+
+      });
+      $('#spinner').fadeOut();
+
+    });
+  };
+  
+  var getPetitionId = function(){
+    $('#form-search').on("submit", function(e){
+      e.preventDefault();
+      var needle = $('#title-search').val(),
+          iframe,
+          host;
+      var result = _.find(petitions, function(p){
+        return p.title === needle;
+      });
+      
+      //Build iframe code
+      iframe = '<iframe src="//' 
+          + document.location.host 
+          + '/widget/' + result.id 
+          + '" style="width: 100%; height: 265px; border: 0;" scrolling="no"></iframe>';
+      $('#generated-widget').val(iframe);
+      
+      // Populate preview area
+      $('#preview-inner').html(iframe);
+    });
+  };
+  
+  loadPetitions();
+  getPetitionId();
+}(jQuery));
