5bf49388a4fa01665d6c324d0677835888ea5497
Douglas Back <douglasback@mac.com>
2013-02-27 21:16:03 -0500

    GRUNT SMASH

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,8 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, jquery:true, indent:2, maxerr:50 */
+/*global _:true */
 (function($){
+  "use strict";
+  
   var petitions, // this will be an array containing the title and ID
       petitionTitles, // used for the typeahead/autocomplete input
       ENDPOINT = 'https://petitions.whitehouse.gov/api/v1/petitions.jsonp';
@@ -30,17 +34,16 @@
     $('#form-search').on("submit", function(e){
       e.preventDefault();
       var needle = $('#title-search').val(),
-          iframe,
-          host;
+          host = document.location.host,
+          iframe;
       var result = _.find(petitions, function(p){
         return p.title === needle;
       });
       
       //Build iframe code
-      iframe = '<iframe src="//' 
-          + document.location.host 
-          + '/widget/' + result.id 
-          + '" style="width: 100%; height: 265px; border: 0;" scrolling="no"></iframe>';
+      iframe = '<iframe src="//' +
+          host + '/widget/' + result.id +
+          '" style="width: 100%; height: 265px; border: 0;" scrolling="no"></iframe>';
       $('#generated-widget').val(iframe);
       
       // Populate preview area
