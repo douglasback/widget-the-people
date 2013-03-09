6854aaa971522a150dd3757366a85c2cb10e7b76
Douglas Back <douglasback@mac.com>
2013-03-08 21:37:33 -0500

    Grunting all the things - New JS built by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,24 +8,18 @@ module.exports = function(grunt) {
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;',
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;*/',
     // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['public/js/vendor/*.js'],
-        dest: 'public/js/lib/<%= pkg.slug %>.js'
-      }
-    },
     uglify: {
       options: {
         banner: '<%= banner %>'
       },
       lib: {
-        src: '<%= concat.dist.dest %>',
+        options: {
+            preserveComments: true,
+            banner: ''
+        },
+        src: 'public/js/vendor/*.js',
         dest: 'public/js/lib/<%= pkg.slug %>.min.js'
       },
       app: {
@@ -56,29 +50,46 @@ module.exports = function(grunt) {
         src: 'Gruntfile.js'
       },
       all: {
-        src: ['public/js/*.js']
+        src: ['public/js/search.js']
       }
     },
-    
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
+    sass: {
+        search: {
+            options: {
+                style: "compact",
+                compass: true
+            
+            },
+            files: [
+                { 
+                    src: 'public/css/scss/styles.scss',
+                    dest: 'public/css/styles.css' 
+                }                
+            ]
+        },
+        widget: {
+            options: {
+                style: "compressed",
+                compass: true
+            
+            },
+            files: [
+                { 
+                    src: 'public/css/scss/widget.scss',
+                    dest: 'public/css/widget.css' 
+                }
+            ]
+        }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'sass']);
 };
